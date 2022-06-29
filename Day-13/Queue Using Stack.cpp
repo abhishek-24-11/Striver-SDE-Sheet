12c195c4228ac68f49ea8496858a279982cd64ab
abhishek-24-11 <107555492+abhishek-24-11@users.noreply.github.com>
2022-06-29 23:12:17 +0530

    Create Queue Using Stack.cpp

---
+++ Day-13/Queue Using Stack.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+class Queue {
+    // Define the data members(if any) here.
+    stack<int> s1;
+    stack<int> s2;
+    
+    public:
+    Queue() {
+        // Initialize your data structure here.
+        
+    }
+
+    void enQueue(int val) {
+        // Implement the enqueue() function.
+        s1.push(val);
+    }
+
+    int deQueue() {
+        // Implement the dequeue() function.
+        if(s1.empty() && s2.empty()){
+            return -1;
+        }
+        if(s2.empty() == false){
+            int poped = s2.top();
+            s2.pop();
+            return poped;
+        }
+        else{
+            while(s1.empty() == false){
+                s2.push(s1.top());
+                s1.pop();
+               
+            }
+             int popped = s2.top();
+                s2.pop();
+                return popped;
+        }
+
+    }
+
+    int peek() {
+        // Implement the peek() function here.
+        if(s1.empty() && s2.empty()){
+            return -1;
+        }
+        if(s2.empty() == false){
+            return s2.top();
+        }
+        else{
+            while(s1.empty() == false){
+                s2.push(s1.top());
+                s1.pop();
+            }
+            return s2.top();
+        }
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function here.
+        return (s1.empty() && s2.empty());
+    }
+};
