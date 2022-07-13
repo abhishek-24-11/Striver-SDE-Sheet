d075e59b3677537a9a0f2f3169262162e0b3e9de
abhishek-24-11 <107555492+abhishek-24-11@users.noreply.github.com>
2022-07-13 22:53:14 +0530

    Create Search In BST.cpp

---
+++ Day-20/Search In BST.cpp
@@ -0,0 +1,13 @@
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    // Write your code here.
+    if(root == NULL) return false;
+    if(root->data == x){
+        return true;
+    }
+    if(root->data > x){
+        return searchInBST(root->left,x);
+    }
+    else if(root->data < x){
+        return searchInBST(root->right,x);
+    }
+}
