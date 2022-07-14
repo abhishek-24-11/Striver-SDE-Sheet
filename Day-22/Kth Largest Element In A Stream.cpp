eaf2dceab3c2d864810774b20b6cba36d2e255b3
abhishek-24-11 <107555492+abhishek-24-11@users.noreply.github.com>
2022-07-14 22:43:18 +0530

    Create Kth Largest Element In A Stream.cpp

---
+++ Day-22/Kth Largest Element In A Stream.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+class Kthlargest {
+public:
+    priority_queue<int,vector<int>,greater<int>> pq;
+    
+    Kthlargest(int k, vector<int> &arr) {
+       // Write your code here.
+        for(int i = 0;i<arr.size();i++){
+            pq.push(arr[i]);
+        }
+    }
+
+    void add(int num) {
+        // Write your code here.
+        if(num <= pq.top()){
+            return;
+        }
+        pq.push(num);
+        pq.pop();
+    }
+
+    int getKthLargest() {
+       // Write your code here.
+        return pq.top();
+    }
+
+};
